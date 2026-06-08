a64b93810f58c5bf82358701a98632886c51a5d3
Thiago <SEU_EMAIL_DO_GITHUB>
2026-06-07 22:13:51 -0300

    Estrutura React Vite

--- App.jsx
+++ App.jsx
@@ -1,3 +1,26 @@
 export default function App() {
-  return <h1>H.J EMPREENDIMENTO</h1>;
+  return (
+    <div style={{padding:'20px', fontFamily:'Arial'}}>
+      <h1>H.J EMPREENDIMENTO</h1>
+
+      <h2>Painel do Patrão</h2>
+
+      <ul>
+        <li>Obras</li>
+        <li>Trabalhadores</li>
+        <li>Presenças</li>
+        <li>Pagamentos</li>
+      </ul>
+
+      <hr />
+
+      <h2>Painel do Trabalhador</h2>
+
+      <p>Obra Atual: Residencial Primavera</p>
+
+      <button>Dia Completo</button>
+      <button style={{marginLeft:'10px'}}>Meio Período</button>
+      <button style={{marginLeft:'10px'}}>Falta</button>
+    </div>
+  )
 }
